90774d33c6e6b31c2cb0c0a07ba35b6246ff573b
Shane Friedman <smoores-gpg@friedmans.us>
2025-08-21 13:06:20 -0400

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,15 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.javascript = {
+    enable = true;
+    corepack.enable = true;
+  };
+}
